fefb7cda1311f77e970c50c130b742a57e6c730b
Dhananjay Yadav <snapdk2@gmail.com>
2024-11-14 15:56:24 +0530

    added mocha test script package dep

--- routes/MainRoutes/Customer.js
+++ routes/MainRoutes/Customer.js
@@ -1,12 +1,9 @@
-const router = require('express').Router();
+const router = require("express").Router();
 
 const customerAuthRoutes = require("../Customer/Authentication");
 const customerEnquiryRoutes = require("../Customer/Enquiry");
 
-
 router.use("/auth", customerAuthRoutes);
-
 router.use("/enquiry", customerEnquiryRoutes);
-
 
 module.exports = router;
