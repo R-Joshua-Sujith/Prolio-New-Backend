cf64fbeb5de007a4ec3ef969ee861d8cc5e31cca
R-Joshua-Sujith <joshuasujith14@gmail.com>
2024-11-19 11:09:36 +0530

    added category apis

--- routes/MainRoutes/Admin.js
+++ routes/MainRoutes/Admin.js
@@ -1,6 +1,8 @@
 const router = require("express").Router();
 const adminAuthRoutes = require("../Admin/Authentication");
+const adminCategoryRoutes = require("../Admin/Category");
 
 router.use("/auth", adminAuthRoutes);
+router.use("/category", adminCategoryRoutes);
 
 module.exports = router;
