037b2d05290e77fcfb75efa3ad9395493b6f3f74
R-Joshua-Sujith <joshuasujith14@gmail.com>
2024-11-23 11:07:44 +0530

    implemented viewing single product

--- controller/Customer/Product.js
+++ controller/Customer/Product.js
@@ -1,9 +1,24 @@
-const ProductModel = require("../../models/Product");
+
 const CustomerModel = require("../../models/Customer");
+const ProductModel = require("../../models/Product");
 
 exports.test = async (req, res) => {
     try {
         res.status(200).json({ message: "Product Success" })
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: "Internal Server Error" })
+    }
+}
+
+exports.getProduct = async (req, res) => {
+    const { slug } = req.params
+    try {
+        const product = await ProductModel.findOne({ "basicDetails.slug": slug }).populate("ownerId", "companyDetails.companyInfo companyDetails.contactInfo")
+        if (!product) {
+            return res.status(400).json({ error: "Product Not Found" })
+        }
+        res.status(200).json(product);
     } catch (error) {
         console.error(error);
         res.status(500).json({ error: "Internal Server Error" })
