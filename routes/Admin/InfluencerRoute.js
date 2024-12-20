d037b391f1d38bec4d865c5ef299da047a094f22
chetankumar007 <chetannooli94@gmail.com>
2024-12-20 13:13:33 +0530

    some changes

--- routes/Admin/InfluencerRoute.js
+++ routes/Admin/InfluencerRoute.js
@@ -1,14 +1,17 @@
 const express = require("express");
 const router = express.Router();
 const { adminVerify } = require("../../controller/Admin/Middleware/auth");
+
 const InfluencerController = require("../../controller/Admin/Influencer");
+const { looseVerify } = require("../../controller/Customer/Middleware/auth");
 
 // Route to get all influencers
 router.get(
   "/all-influencers",
-  adminVerify,
+  looseVerify,
   InfluencerController.getInfluencers
 );
+
 router.patch(
   "/updateInfluencer-status/:influencerId",
   adminVerify,
