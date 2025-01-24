3460bd3df7922e070433583bd098d844233c318d
Dhananjay Yadav <snapdk2@gmail.com>
2025-01-24 10:08:27 +0530

    added some changes

--- models/Logs.js
+++ models/Logs.js
@@ -34,10 +34,64 @@ const LogsSchema = new mongoose.Schema(
       ],
     },
     action: { type: String, required: true },
+    priceValue: [
+      {
+        previousPrice: { type: Number },
+        newPrice: { type: Number },
+      },
+    ],
   },
   {
-    timestamps: true,
+    timestamps: true, // Correctly placed timestamps option
   }
 );
 
 module.exports = mongoose.model("Log", LogsSchema);
+
+
+
+// const mongoose = require("mongoose");
+// const Schema = mongoose.Schema;
+
+// const LogsSchema = new mongoose.Schema(
+//   {
+//     userId: {
+//       type: Schema.Types.ObjectId,
+//       required: true,
+//       refPath: "userModel",
+//     },
+//     userModel: {
+//       type: String,
+//       required: true,
+//       enum: ["Customer", "CompanyUser", "Admin"],
+//     },
+//     targetId: {
+//       type: Schema.Types.ObjectId,
+//       required: true,
+//       refPath: "targetModel",
+//     },
+//     targetModel: {
+//       type: String,
+//       required: true,
+//       enum: [
+//         "Customer",
+//         "Admin",
+//         "Influencer",
+//         "CompanyUser",
+//         "Role",
+//         "Enquiry",
+//         "Product",
+//         "Forum",
+//         "log",
+//       ],
+//     },
+//     action: { type: String, required: true },
+//   },
+//   {
+//     timestamps: true,
+//   }
+// );
+
+// module.exports = mongoose.model("Log", LogsSchema);
+
+
